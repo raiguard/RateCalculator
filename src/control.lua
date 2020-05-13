2de9961e3c29f79693228e6d69af024694bde282
raiguard <caleb.heuer@gmail.com>
2020-05-12 18:50:20 -0600

    Prototype LuaRendering

--- src/control.lua
+++ src/control.lua
@@ -5,10 +5,25 @@ local constants = require("constants")
 local migrations = require("scripts.migrations")
 local player_data = require("scripts.player-data")
 
+local math = math
 local string = string
 
 if __DebugAdapter then
   __DBGPRINT = __DebugAdapter.print
+end
+
+local function round(num, num_decimals)
+  local mult = 10^(num_decimals or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
+local function add_positions(positions)
+  local pos1 = positions[1]
+  for i = 2, #positions do
+    local pos2 = positions[i]
+    pos1 = {x=(pos1.x + pos2.x), y=(pos1.y + pos2.y)}
+  end
+  return pos1
 end
 
 -- -----------------------------------------------------------------------------
@@ -36,52 +51,85 @@ event.on_player_created(function(e)
   player_data.init(e.player_index, game.get_player(e.player_index))
 end)
 
--- PROTOTYPING
+-- ! PROTOTYPING
 
 event.on_player_selected_area(function(e)
-  if e.item == "rcalc-selection-tool" then
-    local player = game.get_player(e.player_index)
-    local entities = e.entities
-    local product_lookup = {}
-    local saved_entities = {}
-    for i = 1, #entities do
-      local entity = entities[i]
-      local recipe = entity.get_recipe()
+  if e.item ~= "rcalc-selection-tool" then return end
 
-      local entity_data = {}
+  local player = game.get_player(e.player_index)
+  local entities = e.entities
+  local product_lookup = {}
+  local saved_entities = {}
 
-      if recipe then
-        local crafting_speed = entity.crafting_speed
-        local productivity_bonus = entity.productivity_bonus
-        local base_unit = (60 / recipe.energy) * crafting_speed * (productivity_bonus + 1)
+  for i = 1, #entities do
+    local entity = entities[i]
+    local recipe = entity.get_recipe()
 
-        local products = {}
-        for product_index, product in ipairs(recipe.products) do
-          -- TODO handle min and max amounts, probabilities
-          local rate_per_minute = product.amount * base_unit
-          product.rate_per_minute = rate_per_minute
-          products[product_index] = product
+    local entity_data = {}
 
-          local lookup_data = product_lookup[product.name]
-          if lookup_data then
-            product_lookup[product.name] = lookup_data + rate_per_minute
-          else
-            product_lookup[product.name] = rate_per_minute
-          end
+    if recipe then
+      local crafting_speed = entity.crafting_speed
+      local productivity_bonus = entity.productivity_bonus
+      local base_unit = (60 / recipe.energy) * crafting_speed * (productivity_bonus + 1)
+
+      local products = {}
+      for product_index, product in ipairs(recipe.products) do
+        -- TODO handle min and max amounts, probabilities
+        local rate_per_minute = product.amount * base_unit
+        product.rate_per_minute = rate_per_minute
+        products[product_index] = product
+
+        local lookup_data = product_lookup[product.name]
+        if lookup_data then
+          product_lookup[product.name] = lookup_data + rate_per_minute
+        else
+          product_lookup[product.name] = rate_per_minute
         end
 
-        entity_data.products = products
-      else
-        -- TODO
+        -- ! PROTOTYPE RENDERING
+        local box = entity.bounding_box
+        -- local width = box.right_bottom.x - box.left_top.x
+        -- local height = box.right_bottom.y - box.left_top.y
+        -- local background_scale = 0.8 * math.min(width, height)
+        local initial_offset = {x=0.4, y=0.25}
+        local offset = product_index - 1
+        -- TODO fixed precision format
+        rendering.draw_rectangle{
+          color = {10,10,10,200},
+          filled = true,
+          left_top = add_positions{box.left_top, initial_offset, {x=-0.35, y=-0.35}, {x=0, y=(offset * 0.5)}},
+          right_bottom = add_positions{box.left_top, initial_offset, {x=0.1, y=0.1}, {x=0, y=(offset * 0.5)}, {x=2, y=0.3}},
+          surface = entity.surface,
+          time_to_live = 120
+        }
+        rendering.bring_to_front(rendering.draw_sprite{
+          sprite = product.type.."/"..product.name,
+          target = add_positions{box.left_top, initial_offset, {x=0, y=(offset * 0.5)}},
+          surface = entity.surface,
+          time_to_live = 120,
+          x_scale = 0.6,
+          y_scale = 0.6
+        })
+        rendering.draw_text{
+          text = round(product.rate_per_minute, 3).." / m",
+          surface = entity.surface,
+          target = add_positions{box.left_top, initial_offset, {x=0.4, y=-0.3}, {x=0, y=(offset * 0.5)}},
+          color = {255,255,255},
+          time_to_live = 120
+        }
       end
 
-      entity_data.entity = entity
-      saved_entities[entity.unit_number] = entity_data
+      entity_data.products = products
+    else
+      -- TODO
     end
-    local breakpoint
+
+    entity_data.entity = entity
+    saved_entities[entity.unit_number] = entity_data
   end
+  local breakpoint
 end)
 
 event.on_player_alt_selected_area(function(e)
-
+  -- TODO cancel selection
 end)
--- src/scripts/player-data.lua
+++ src/scripts/player-data.lua
@@ -2,25 +2,26 @@ local player_data = {}
 
 function player_data.init(index, player)
   global.players[index] = {
-    dictionary = {},
     flags = {},
     gui = {},
-    regions = {},
-    settings = {}
+    regions = {
+      __nextindex = 0
+    },
+    settings = {},
+    translations = {}
   }
 
   player_data.refresh(player, global.players[index])
 end
 
 function player_data.import_settings(player, player_table)
-  local settings = {}
+  local settings = player_table.settings
   -- TODO
-
-  player_table.settings = settings
 end
 
 function player_data.refresh(player, player_table)
   -- TODO: close all GUIs
+
   -- refresh settings
   player_data.import_settings(player, player_table)
 end
