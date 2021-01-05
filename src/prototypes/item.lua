ecf95ef3f0f524a176bd64ee41486e48dbd7ed3f
raiguard <caleb.heuer@gmail.com>
2021-01-04 19:09:24 -0700

    Fix selection of ghosts (#55)

--- src/prototypes/item.lua
+++ src/prototypes/item.lua
@@ -10,8 +10,8 @@ local selection_tool_base = {
     {icon = data_util.black_image, icon_size = 1, scale = 64},
     {icon = "__RateCalculator__/graphics/selection-tool.png", icon_size = 32, mipmap_count = 2}
   },
-  selection_mode = {"any-entity", "friend", "trees"},
-  alt_selection_mode = {"any-entity", "friend", "trees"},
+  selection_mode = {"buildable-type", "friend", "trees"},
+  alt_selection_mode = {"buildable-type", "friend", "trees"},
   stack_size = 1,
   flags = {"hidden", "only-in-cursor", "not-stackable"},
   draw_label_for_cursor_render = true
