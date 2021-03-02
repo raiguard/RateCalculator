8a5866340798650113e2fc6098625db5c9aa93c2
raiguard <caleb.heuer@gmail.com>
2021-03-02 14:43:23 -0700

    Fix lab calc not reading research speed (#61)

--- src/scripts/calc/lab.lua
+++ src/scripts/calc/lab.lua
@@ -2,14 +2,15 @@ local calc_util = require("scripts.calc.util")
 
 return function(rates, entity, emissions_per_second, prototypes, research_data)
   if research_data then
+    local research_multiplier = research_data.multiplier
+    local researching_speed = entity.prototype.researching_speed
+    local speed_modifier = research_data.speed_modifier
     --[[
       due to a bug with entity_speed_bonus, we must subtract the force's lab speed bonus and convert it to a
       multiplicative relationship
     ]]
     local lab_multiplier = (
-      research_data.multiplier * (
-        (entity.speed_bonus + 1 - research_data.speed_modifier) * (research_data.speed_modifier + 1)
-      )
+      research_multiplier * ((entity.speed_bonus + 1 - speed_modifier) * (speed_modifier + 1)) * researching_speed
     )
 
     for _, ingredient in ipairs(research_data.ingredients) do
@@ -23,4 +24,3 @@ return function(rates, entity, emissions_per_second, prototypes, research_data)
 
   return emissions_per_second
 end
-
