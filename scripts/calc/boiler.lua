da9c4b4f38ebc9bc3b2803a13705dd5c35caf02d
Caleb Heuer <ch@raiguard.me>
2022-10-27 20:35:46 -0600

    Fix boiler calculations with heat capacities

--- scripts/calc/boiler.lua
+++ scripts/calc/boiler.lua
@@ -1,27 +1,47 @@
 local calc_util = require("__RateCalculator__.scripts.calc.util")
 
-local function calc_fluidbox_rate(entity_prototype, fluid_prototype)
-  local energy_per_amount = (entity_prototype.target_temperature - fluid_prototype.default_temperature)
-    * fluid_prototype.heat_capacity
-  local max_consumption = entity_prototype.max_energy_usage / energy_per_amount
-  return max_consumption * 60
+--- @param fluidbox LuaFluidBox
+--- @param index uint
+--- @return LuaFluidPrototype?
+local function get_fluid(fluidbox, index)
+  local fluid = fluidbox.get_filter(index)
+  if not fluid then
+    fluid = fluidbox[index] --[[@as FluidBoxFilter?]]
+  end
+  if fluid then
+    return game.fluid_prototypes[fluid.name]
+  end
 end
 
-return function(rates, entity, emissions_per_second, prototypes)
+--- @param entity LuaEntity
+return function(rates, entity, emissions_per_second)
   local entity_prototype = entity.prototype
+  local fluidbox = entity.fluidbox
 
-  for i, rate_kind in ipairs({ "input", "output" }) do
-    local fluid = entity.fluidbox.get_filter(i)
-    if fluid then
-      local fluid_prototype = prototypes.fluid[fluid.name]
+  local input_fluid = get_fluid(fluidbox, 1)
+  if input_fluid then
+    local minimum_temperature = fluidbox.get_prototype(1).minimum_temperature or input_fluid.default_temperature
+    local energy_per_amount = (entity_prototype.target_temperature - minimum_temperature) * input_fluid.heat_capacity
+    local fluid_usage = entity_prototype.max_energy_usage / energy_per_amount * 60
+    calc_util.add_rate(
+      rates.materials,
+      "input",
+      "fluid",
+      input_fluid.name,
+      input_fluid.localised_name,
+      fluid_usage,
+      "entity/" .. entity.name
+    )
+
+    local output_fluid = get_fluid(fluidbox, 2)
+    if output_fluid then
       calc_util.add_rate(
         rates.materials,
-        rate_kind,
+        "output",
         "fluid",
-        fluid.name,
-        fluid_prototype.localised_name,
-        calc_fluidbox_rate(entity_prototype, fluid_prototype),
-        nil,
+        output_fluid.name,
+        output_fluid.localised_name,
+        fluid_usage,
         "entity/" .. entity.name
       )
     end
