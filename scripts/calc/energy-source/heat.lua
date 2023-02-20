8fad7a1b897f2a60a3985d8c335f692c1e440440
Caleb Heuer <ch@raiguard.me>
2023-02-19 21:12:59 -0700

    Account for efficiency bonus when calculating heat

--- scripts/calc/energy-source/heat.lua
+++ scripts/calc/energy-source/heat.lua
@@ -1,7 +1,15 @@
 local calc_util = require("__RateCalculator__.scripts.calc.util")
 
+--- @param entity LuaEntity
 return function(rates, entity, emissions_per_second)
-  calc_util.add_rate(rates, "input", "entity", entity.name, entity.localised_name, entity.prototype.max_energy_usage)
+  calc_util.add_rate(
+    rates,
+    "input",
+    "entity",
+    entity.name,
+    entity.localised_name,
+    entity.prototype.max_energy_usage * (1 + entity.consumption_bonus)
+  )
 
   -- From testing, it appears that heat energy sources never produce pollution
   return emissions_per_second
