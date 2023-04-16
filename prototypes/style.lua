54ec3f701e2d9bf6ff64e27e5ae99ccd4d8d5617
Caleb Heuer <ch@raiguard.me>
2023-04-15 19:17:45 -0600

    Remove unused styles

--- prototypes/style.lua
+++ prototypes/style.lua
@@ -7,31 +7,6 @@ styles.rcalc_units_choose_elem_button = {
   width = 30,
 }
 
-styles.rcalc_warning_flow = {
-  type = "horizontal_flow_style",
-  padding = 12,
-  height = 38,
-  horizontal_align = "center",
-  vertical_align = "center",
-  vertical_spacing = 8,
-  horizontally_stretchable = "on",
-  vertically_stretchable = "on",
-}
-
-styles.rcalc_warning_frame_in_shallow_frame = {
-  type = "frame_style",
-  parent = "deep_frame_in_shallow_frame",
-  graphical_set = {
-    base = {
-      position = { 85, 0 },
-      corner_size = 8,
-      center = { position = { 411, 25 }, size = { 1, 1 } },
-      draw_type = "outer",
-    },
-    shadow = default_inner_shadow,
-  },
-}
-
 styles.rcalc_slot_button_filtered = {
   type = "button_style",
   parent = "transparent_slot",
