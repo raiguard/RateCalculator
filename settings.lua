b3fc92a0f2443144aa9fcd68091e4da48c6dcab6
Caleb Heuer <ch@raiguard.me>
2023-04-24 13:45:23 -0600

    Remove unused things

--- settings.lua
+++ settings.lua
@@ -1,13 +1,5 @@
 data:extend({
   {
-    type = "int-setting",
-    name = "rcalc-entities-per-tick",
-    setting_type = "runtime-global",
-    default_value = 10,
-    minimum_value = 1,
-    maximum_value = 50,
-  },
-  {
     type = "bool-setting",
     name = "rcalc-dismiss-tool-on-selection",
     setting_type = "runtime-per-user",
