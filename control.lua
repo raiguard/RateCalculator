2dbd34e42c621da9330ca58c27f902f0a19cb54d
Caleb Heuer <ch@raiguard.me>
2023-06-17 13:02:26 -0600

    Clean up control.lua

--- control.lua
+++ control.lua
@@ -1,10 +1,12 @@
 local handler = require("__core__/lualib/event_handler")
 
-handler.add_lib(require("__flib__/dictionary-lite"))
-handler.add_lib(require("__flib__/gui-lite"))
+handler.add_libraries({
+  require("__flib__/dictionary-lite"),
+  require("__flib__/gui-lite"),
 
-handler.add_lib(require("__RateCalculator__/scripts/migrations"))
+  require("__RateCalculator__/scripts/migrations"),
 
-handler.add_lib(require("__RateCalculator__/scripts/calc"))
-handler.add_lib(require("__RateCalculator__/scripts/gui"))
-handler.add_lib(require("__RateCalculator__/scripts/shortcut"))
+  require("__RateCalculator__/scripts/calc"),
+  require("__RateCalculator__/scripts/gui"),
+  require("__RateCalculator__/scripts/shortcut"),
+})
