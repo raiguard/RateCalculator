819e28ed63fe4d8df2943b0e2c6623e404640497
Caleb Heuer <ch@raiguard.me>
2024-01-29 11:46:10 -0700

    Fix shortcut icon sizes

--- prototypes/shortcut.lua
+++ prototypes/shortcut.lua
@@ -7,6 +7,8 @@ data:extend({
     disabled_icon = "__RateCalculator__/graphics/shortcut-x32-white.png",
     small_icon = "__RateCalculator__/graphics/shortcut-x24-black.png",
     disabled_small_icon = "__RateCalculator__/graphics/shortcut-x24-white.png",
+    icon_size = 32,
+    small_icon_size = 24,
     action = "lua",
     associated_control_input = "rcalc-get-selection-tool",
   },
