c08b3d32ebdabe865acaa816567d240660f7e52d
Caleb Heuer <ch@raiguard.me>
2024-10-29 21:16:09 -0600

    Remove rocket silo craft time jank
    Fixes #161

--- scripts/calc-util.lua
+++ scripts/calc-util.lua
@@ -85,52 +85,6 @@ function calc_util.add_rate(set, category, type, name, quality, amount, invert, 
   end
 end
 
---- Source: https://github.com/ClaudeMetz/FactoryPlanner/blob/0f0aeae03386f78290d932cf51130bbcb2afa83d/modfiles/data/handlers/generator_util.lua#L364
---- @param prototype LuaEntityPrototype
---- @return number?
-function calc_util.get_seconds_per_rocket_launch(prototype)
-  local rocket_prototype = prototype.rocket_entity_prototype
-  if not rocket_prototype then
-    return nil
-  end
-
-  local rocket_flight_threshold = 0.5 -- hardcoded in the game files
-  local launch_steps = {
-    lights_blinking_open = (1 / prototype.light_blinking_speed) + 1,
-    doors_opening = (1 / prototype.door_opening_speed) + 1,
-    doors_opened = prototype.rocket_rising_delay + 1,
-    rocket_rising = (1 / rocket_prototype.rising_speed) + 1,
-    rocket_ready = 14, -- estimate for satellite insertion delay
-    launch_started = prototype.launch_wait_time + 1,
-    engine_starting = (1 / rocket_prototype.engine_starting_speed) + 1,
-    -- This calculates a fractional amount of ticks. Also, math.log(x) calculates the natural logarithm
-    rocket_flying = math.log(
-      1 + rocket_flight_threshold * rocket_prototype.flying_acceleration / rocket_prototype.flying_speed
-    ) / math.log(1 + rocket_prototype.flying_acceleration),
-    lights_blinking_close = (1 / prototype.light_blinking_speed) + 1,
-    doors_closing = (1 / prototype.door_opening_speed) + 1,
-  }
-
-  local total_ticks = 0
-  for _, ticks_taken in pairs(launch_steps) do
-    total_ticks = total_ticks + ticks_taken
-  end
-
-  return (total_ticks / 60) -- retured value is in seconds
-end
-
---- @param entity LuaEntity
---- @param crafts_per_second double
---- @return double
-function calc_util.get_rocket_adjusted_crafts_per_second(entity, crafts_per_second)
-  local prototype = entity.prototype
-  local seconds_per_launch = calc_util.get_seconds_per_rocket_launch(prototype)
-  local normal_crafts = prototype.rocket_parts_required
-  local missed_crafts = seconds_per_launch * crafts_per_second * (entity.productivity_bonus + 1)
-  local ratio = normal_crafts / (normal_crafts + missed_crafts)
-  return crafts_per_second * ratio
-end
-
 --- @param set CalculationSet
 --- @param entity LuaEntity
 --- @param invert boolean
@@ -276,11 +230,6 @@ function calc_util.process_crafter(set, entity, invert, emissions_per_second)
   --- @cast quality -?
 
   local crafts_per_second = entity.crafting_speed / recipe.energy
-  -- Rocket silos will lose time to the launch animation
-  -- TODO: Update for 2.0 rocket silo craft-while-launching
-  if entity.type == "rocket-silo" then
-    crafts_per_second = calc_util.get_rocket_adjusted_crafts_per_second(entity, crafts_per_second)
-  end
 
   for _, ingredient in pairs(recipe.ingredients) do
     local amount = ingredient.amount * crafts_per_second
