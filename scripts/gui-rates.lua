96872b593baefe2c5bfa82ec5eae1f16bfcc3ae0
Caleb Heuer <ch@raiguard.me>
2025-06-14 20:46:09 -0600

    Micro

--- scripts/gui-rates.lua
+++ scripts/gui-rates.lua
@@ -399,10 +399,10 @@ end
 
 local unit_lookup = {
   ["rcalc-electric-energy-buffer-dummy"] = "joule",
+  ["rcalc-electric-power-dummy"] = "watt",
   ["rcalc-fluid-fuel-dummy"] = "watt",
   ["rcalc-heat-dummy"] = "watt",
   ["rcalc-item-fuel-dummy"] = "watt",
-  ["rcalc-electric-power-dummy"] = "watt",
   ["rcalc-thrust-dummy"] = "newton",
 }
 
