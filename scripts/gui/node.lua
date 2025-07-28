8a6189cd7f27f8a3ffcc705ac168090ccbc8f8b1
Caleb Heuer <ch@raiguard.me>
2025-07-28 13:57:22 -0600

    Show net rates

--- scripts/gui/node.lua
+++ scripts/gui/node.lua
@@ -29,6 +29,9 @@ local node_gui = {}
 local mt = { __index = node_gui }
 script.register_metatable("node_gui", mt)
 
+local green = { r = 0.58, g = 1, b = 0.58 }
+local red = { r = 1, g = 0.58, b = 0.58 }
+
 --- @param main_gui MainGui
 --- @param parent LuaGuiElement
 --- @param node MaterialNode
@@ -40,7 +43,7 @@ function node_gui.new(main_gui, parent, node)
   flow.style.vertical_align = "center"
   local button_holder = flow.add({ type = "empty-widget" })
   button_holder.style.size = 40
-  local button = button_holder.add({
+  button_holder.add({
     type = "sprite-button",
     style = "flib_standalone_slot_button_default",
     sprite = button_desc.sprite,
@@ -54,15 +57,39 @@ function node_gui.new(main_gui, parent, node)
     qualifier_label.style.left_padding = 2
     qualifier_label.style.top_padding = -2
   end
+  local text_holder = flow
+  if node:get_gui_category() == "intermediate" then
+    local intermediate_flow = flow.add({ type = "flow", style = "packed_vertical_flow", direction = "vertical" })
+    text_holder = intermediate_flow.add({ type = "flow", style = "player_input_horizontal_flow" })
+    intermediate_flow.style.top_margin = -3
+
+    local net_rate = node.output.amount + node.input.amount
+    local net_rate_label = intermediate_flow.add({
+      type = "label",
+      style = "semibold_label",
+      caption = {
+        "",
+        "= ",
+        net_rate > 0 and "+" or "",
+        format_rate(net_rate, description.number_format or { factor = 1, unit = "/s" }),
+      },
+    })
+    net_rate_label.style.top_margin = -6
+    if net_rate > 0 then
+      net_rate_label.style.font_color = green
+    elseif net_rate < 0 then
+      net_rate_label.style.font_color = red
+    end
+  end
   if next(node.output.configurations) then
     local text = format_rate(node.output.amount, description.number_format or { factor = 1, unit = "/s" })
-    local label = flow.add({ type = "label", style = "semibold_label", caption = text })
-    label.style.font_color = { r = 0.58, g = 1, b = 0.58 }
+    local label = text_holder.add({ type = "label", style = "semibold_label", caption = text })
+    label.style.font_color = green
   end
   if next(node.input.configurations) then
     local text = format_rate(node.input.amount, description.number_format or { factor = 1, unit = "/s" })
-    local label = flow.add({ type = "label", style = "semibold_label", caption = text })
-    label.style.font_color = { r = 1, g = 0.58, b = 0.58 }
+    local label = text_holder.add({ type = "label", style = "semibold_label", caption = text })
+    label.style.font_color = red
   end
   log(serpent.block(description))
 
